250c8a7e0c64730a911765ea58a8384607456660
Suheyl Turac <skbsturac@live.com>
2018-10-04 14:44:46 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,16 @@
 def hello_t(array)
-  array = []
-  array.each
+  # call your method here!
+  if block_given?
+    i = 0 
+  
+    while i < array.length 
+     yield array[i]
+      i = i + 1 
+    end 
+  
+    array 
+  else 
+    puts "Hey! No block was given!"
+  end 
 end
-return array
-end
-
-# call your method here!
 
